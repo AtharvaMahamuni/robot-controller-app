53819b38f516bbfa6051ea5e73269fbe3d330904
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-08-05 16:01:26 +0530

    Buttons and sliders added. Edited first screen

--- app/src/main/java/com/example/arccontroller/MainActivity.java
+++ app/src/main/java/com/example/arccontroller/MainActivity.java
@@ -16,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         /*this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_main);
-        connect=(Button)findViewById(R.id.Connect);
-    }
-    public void onClick(View v){
-        Intent i=new Intent(MainActivity.this,MainActivity0.class);
-        startActivity(i);
+        connect=(Button)findViewById(R.id.connect);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,MainActivity0.class);
+                startActivity(i);
+            }
+        });
     }
 }
