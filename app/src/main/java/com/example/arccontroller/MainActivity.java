0a0153594f196d08231346e8c81139867bb69212
Mark-1707 <shiroteomkar@gmail.com>
2020-08-05 09:16:23 +0530

    Activities for ConnectionSetUp  and Controller

--- app/src/main/java/com/example/arccontroller/MainActivity.java
+++ app/src/main/java/com/example/arccontroller/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.arccontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /*this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_main);
-
-        
+        connect=(Button)findViewById(R.id.Connect);
+    }
+    public void onClick(View v){
+        Intent i=new Intent(MainActivity.this,MainActivity0.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/arccontroller/MainActivity0.java
@@ -0,0 +1,18 @@
+package com.example.arccontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;;
+import android.view.Window;
+
+public class MainActivity0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main0);
+
+    }
+
+}
