2bd8ca31189b342dbef0bc4a7884ebfffc1528c4
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-08-06 14:25:57 +0530

    New assets are added

--- app/src/main/java/com/example/arccontroller/MainActivity0.java
+++ app/src/main/java/com/example/arccontroller/MainActivity0.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity0 extends AppCompatActivity {
+
+    ImageView right, left, up, down;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main0);
+
+        right = findViewById(R.id.right);
+        left = findViewById(R.id.left);
+        up = findViewById(R.id.up);
+        down = findViewById(R.id.down);
+
+
+
 
     }
 
