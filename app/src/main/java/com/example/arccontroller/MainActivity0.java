eaee2a0caef562dffa3e456d45c4f7bf21e115a6
Mark-1707 <shiroteomkar@gmail.com>
2020-08-06 18:40:39 +0530

    Touch Action set(Faint)

--- app/src/main/java/com/example/arccontroller/MainActivity0.java
+++ app/src/main/java/com/example/arccontroller/MainActivity0.java
@@ -3,6 +3,9 @@ package com.example.arccontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
@@ -22,9 +25,80 @@ public class MainActivity0 extends AppCompatActivity {
         up = findViewById(R.id.up);
         down = findViewById(R.id.down);
 
+        up.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("IMAGE","motion event:"+event.toString());
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN: {
+                        up.setImageResource(R.drawable.ic_arrow_drop_up_faint);
+                        return true;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        up.setImageResource(R.drawable.ic_arrow_drop_up);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
 
 
+        down.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("IMAGE","motion event:"+event.toString());
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN: {
+                        down.setImageResource(R.drawable.ic_arrow_drop_down_faint);
+                        return true;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        down.setImageResource(R.drawable.ic_arrow_drop_down);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
 
+
+        left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("IMAGE","motion event:"+event.toString());
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN: {
+                        left.setImageResource(R.drawable.ic_arrow_drop_left_faint);
+                        return true;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        left.setImageResource(R.drawable.ic_chevron_left_48px);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("IMAGE","motion event:"+event.toString());
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN: {
+                        right.setImageResource(R.drawable.ic_arrow_drop_right_faint);
+                        return true;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        right.setImageResource(R.drawable.ic_chevron_right);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
 }
