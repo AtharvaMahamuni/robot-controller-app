8ec4d0eaa281125f91abee2311be5f55a7667f19
Mark-1707 <shiroteomkar6@gmail.com>
2020-09-23 22:30:00 +0530

    AdMob integration

--- app/src/main/java/com/example/arccontroller/MainActivity.java
+++ app/src/main/java/com/example/arccontroller/MainActivity.java
@@ -10,10 +10,17 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.material.shape.InterpolateOnScrollPositionChangeHelper;
+
 public class MainActivity extends AppCompatActivity {
     Button connect;
     BluetoothAdapter bluetoothAdapter;
-
+    InterstitialAd interstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
         /*this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_main);
         connect=(Button)findViewById(R.id.connect);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+
+        interstitialAd=new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-7747416735170970/1613819677");
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+
 
         bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter==null){
@@ -33,8 +51,14 @@ public class MainActivity extends AppCompatActivity {
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent i=new Intent(MainActivity.this,Bluetooth.class);
                 startActivity(i);
+                if(interstitialAd.isLoaded()){
+                    interstitialAd.show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Ad Not loaded",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
